186c5f5bc6413862ed20c63c09b47b837cb066e6
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-06-14 02:31:41 +0900

    Update pyKyobo_JeongboGineung_.py

--- pyKyobo_JeongboGineung_.py
+++ pyKyobo_JeongboGineung_.py
@@ -44,26 +44,24 @@ book_data = []
 tracks = soup.select(".prod_list .prod_item")
 
 for track in tracks:
-    spans = track.select(".auto_overflow_inner span")
+    spans = track.select(".prod_info span")
     if len(spans) >= 2:  # 두 번째 span 요소가 있는지 확인
         title = spans[1].text.strip()
-        author = track.select_one(".auto_overflow_inner a.author.rep").text.strip()
+        author = track.select_one(".prod_publish .text").text.strip()
         price = track.select_one(".price span").text.strip()
         # 이미지 요소가 로드될 때까지 대기
         image_element = track.select_one(".img_box .prod_img_load")  # 이미지 요소 가져오기
         image_url = image_element.get('src') if image_element else None  # src에서 이미지 URL 가져오기
-    
+        
         link_element = track.select_one(".auto_overflow_inner a")  # 링크 요소 가져오기
         href = link_element.get('href') if link_element else None  # href 속성 가져오기
-
-
+    
         book_data.append({
             "title": title,
             "imageURL": image_url,
             "author": author,
             "price" : price,
-            "url" : href
-        })
+            "url
 
 print(book_data)
 
