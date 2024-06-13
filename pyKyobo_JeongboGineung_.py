44e95ed718461d8f288acb9e3cebe50915c07ed8
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-06-14 02:38:45 +0900

    Update pyKyobo_JeongboGineung_.py

--- pyKyobo_JeongboGineung_.py
+++ pyKyobo_JeongboGineung_.py
@@ -12,11 +12,12 @@ import json
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
-filename = f"Kyobo/JeongboGineung/JeongboGineung_{current_date}.json"
+filename = f"Kyobo/Geonseol/Geonseol_{current_date}.json"
+
 
 # 웹드라이버 설치
-options = ChromeOptions()
-options.add_argument("--start-maximized")
+options = ChromeOptions() 
+options.add_argument("--start-maximized") 
 options.add_argument("--headless")  # Headless 모드 추가
 options.add_argument("--no-sandbox")  # Sandbox 모드 비활성화
 options.add_argument("--disable-dev-shm-usage")  # /dev/shm 사용 비활성화
